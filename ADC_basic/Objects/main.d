c46340c93931b4dc20400bfbd9fde1685723f953
suresh33661 <sureshpragada3366@gmail.com>
2025-12-12 15:17:43 +0530

    Embedded Projecs and Drivers

---
+++ ADC_basic/Objects/main.d
@@ -0,0 +1,5 @@
+./objects/main.o: main.c \
+  D:\ARM\Keil\TM4C_DFP\1.1.0\Device\Include\TM4C123\TM4C123.h \
+  D:\ARM\Keil\TM4C_DFP\1.1.0\Device\Include\TM4C123\TM4C123GH6PM.h \
+  D:\ARM\ARM\CMSIS\6.2.0\CMSIS\Core\Include\core_cm4.h \
+  D:\ARM\Keil\TM4C_DFP\1.1.0\Device\Include\TM4C123\system_TM4C123.h
---
+++ ADC_basic/Objects/system_tm4c123.d
@@ -0,0 +1,5 @@
+./objects/system_tm4c123.o: RTE\Device\TM4C123GH6PM\system_TM4C123.c \
+  D:\ARM\Keil\TM4C_DFP\1.1.0\Device\Include\TM4C123\TM4C123.h \
+  D:\ARM\Keil\TM4C_DFP\1.1.0\Device\Include\TM4C123\TM4C123GH6PM.h \
+  D:\ARM\ARM\CMSIS\6.2.0\CMSIS\Core\Include\core_cm4.h \
+  D:\ARM\Keil\TM4C_DFP\1.1.0\Device\Include\TM4C123\system_TM4C123.h
